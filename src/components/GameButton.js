4f8c9de223910842ead10f267d724d81b340b19b
mcornale <massimiliano.cornale@gmail.com>
2021-11-17 11:56:25 +0100

    Edit grid game style and buttons

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -1,5 +1,10 @@
 const GameButton = () => {
-  return <button className='game-button'></button>;
+  return (
+    <div className='game-button-container'>
+      <button className='game-button-cover'></button>
+      <button className='game-button'></button>
+    </div>
+  );
 };
 
 export default GameButton;
--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -6,7 +6,11 @@ const GameGrid = () => {
 
   for (let i = 0; i < gridSize; i++) gridElements.push(<GameButton key={i} />);
 
-  return <div className='game-grid'>{gridElements}</div>;
+  return (
+    <div className={`game-grid game-grid-${gridSize === 16 ? '4x4' : '6x6'}`}>
+      {gridElements}
+    </div>
+  );
 };
 
 export default GameGrid;
