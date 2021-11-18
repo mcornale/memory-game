d1fb6081c34ed3c9457b15fafd097790d34bfb94
mcornale <massimiliano.cornale@gmail.com>
2021-11-18 12:03:04 +0100

    Allow user to change game settings from menu

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,53 +1,85 @@
 import Button from './Button';
 
 import { MAX_NUM_OF_PLAYERS, GAME_GRID_SIZES, GAME_THEMES } from '../constants';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setTheme,
+  setNumOfPlayers,
+  setGridSize,
+} from '../store/gameSettingsSlice';
+import firstCapitalLetter from '../helpers/firstCapitalLetter';
 
 const GameMenu = (props) => {
   const gridSize = useSelector((state) => state.gameSettings.gridSize);
   const numOfPlayers = useSelector((state) => state.gameSettings.numOfPlayers);
   const gridTheme = useSelector((state) => state.gameSettings.gridTheme);
+  const dispatch = useDispatch();
 
   const themeButtons = [];
   const numOfPlayersButtons = [];
   const gridSizeButtons = [];
 
-  for (const key in GAME_THEMES) {
-    themeButtons.push(
-      <Button
-        key={key}
-        type={`menu-selection${
-          GAME_THEMES[key] === gridTheme ? '-active' : ''
-        }`}
-      >
-        {GAME_THEMES[key][0].toUpperCase() + GAME_THEMES[key].slice(1)}
-      </Button>
-    );
-  }
+  const onChangeThemeHandler = (newGameTheme) => {
+    dispatch(setTheme(newGameTheme));
+  };
 
-  for (let i = 0; i < MAX_NUM_OF_PLAYERS; i++) {
-    numOfPlayersButtons.push(
-      <Button
-        key={i}
-        type={`menu-selection${i + 1 === numOfPlayers ? '-active' : ''}`}
-      >
-        {i + 1}
-      </Button>
-    );
-  }
+  const onChangeNumOfPlayersHandler = (newNumOfPlayers) => {
+    dispatch(setNumOfPlayers(newNumOfPlayers));
+  };
 
-  for (const key in GAME_GRID_SIZES) {
-    gridSizeButtons.push(
-      <Button
-        key={key}
-        type={`menu-selection${
-          GAME_GRID_SIZES[key] === gridSize ? '-active' : ''
-        }`}
-      >
-        {key}
-      </Button>
-    );
-  }
+  const onChangeGridSizeHandler = (newGridSize) => {
+    dispatch(setGridSize(newGridSize));
+  };
+
+  const generateThemeButtons = () => {
+    for (const key in GAME_THEMES) {
+      themeButtons.push(
+        <Button
+          onClick={onChangeThemeHandler.bind(null, GAME_THEMES[key])}
+          key={key}
+          type={`menu-selection${
+            GAME_THEMES[key] === gridTheme ? '-active' : ''
+          }`}
+        >
+          {firstCapitalLetter(GAME_THEMES[key])}
+        </Button>
+      );
+    }
+  };
+
+  const generateNumOfPlayersButtons = () => {
+    for (let i = 0; i < MAX_NUM_OF_PLAYERS; i++) {
+      numOfPlayersButtons.push(
+        <Button
+          onClick={onChangeNumOfPlayersHandler.bind(null, i + 1)}
+          key={i}
+          type={`menu-selection${i + 1 === numOfPlayers ? '-active' : ''}`}
+        >
+          {i + 1}
+        </Button>
+      );
+    }
+  };
+
+  const generateGridSizeButtons = () => {
+    for (const key in GAME_GRID_SIZES) {
+      gridSizeButtons.push(
+        <Button
+          onClick={onChangeGridSizeHandler.bind(null, GAME_GRID_SIZES[key])}
+          key={key}
+          type={`menu-selection${
+            GAME_GRID_SIZES[key] === gridSize ? '-active' : ''
+          }`}
+        >
+          {key}
+        </Button>
+      );
+    }
+  };
+
+  generateThemeButtons();
+  generateNumOfPlayersButtons();
+  generateGridSizeButtons();
 
   return (
     <div className='game-menu'>
---
+++ src/helpers/firstCapitalLetter.js
@@ -0,0 +1,5 @@
+const firstCapitalLetter = (str) => {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+export default firstCapitalLetter;
