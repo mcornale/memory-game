3d2a1303e7df0e51f6eec405fc73bcdba973ee7e
mcornale <massimiliano.cornale@gmail.com>
2021-11-18 11:40:37 +0100

    Add dynamic buttons generation in game menu

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,25 +1,49 @@
 import Button from './Button';
 
+import { MAX_NUM_OF_PLAYERS, GAME_GRID_SIZES, GAME_THEMES } from '../constants';
+import { useSelector } from 'react-redux';
+
 const GameMenu = (props) => {
+  const gridSize = useSelector((state) => state.gameSettings.gridSize);
+  const numOfPlayers = useSelector((state) => state.gameSettings.numOfPlayers);
+  const gridTheme = useSelector((state) => state.gameSettings.gridTheme);
+
+  const themeButtons = [];
+  const numOfPlayersButtons = [];
+  const gridSizeButtons = [];
+
+  for (const key in GAME_THEMES) {
+    themeButtons.push(
+      <Button key={key} type='menu-selection'>
+        {GAME_THEMES[key][0].toUpperCase() + GAME_THEMES[key].slice(1)}
+      </Button>
+    );
+  }
+
+  for (let i = 0; i < MAX_NUM_OF_PLAYERS; i++) {
+    numOfPlayersButtons.push(
+      <Button key={i} type='menu-selection'>
+        {i + 1}
+      </Button>
+    );
+  }
+
+  for (const key in GAME_GRID_SIZES) {
+    gridSizeButtons.push(
+      <Button key={key} type='menu-selection'>
+        {key}
+      </Button>
+    );
+  }
+
   return (
     <div className='game-menu'>
       <h3>Select Theme</h3>
-      <div className='game-menu-buttons'>
-        <Button type='menu-selection'>Numbers</Button>
-        <Button type='menu-selection'>Icons</Button>
-      </div>
+      <div className='game-menu-buttons'>{themeButtons}</div>
       <h3>Numbers of Players</h3>
-      <div className='game-menu-buttons'>
-        <Button type='menu-selection'>1</Button>
-        <Button type='menu-selection'>2</Button>
-        <Button type='menu-selection'>3</Button>
-        <Button type='menu-selection'>4</Button>
-      </div>
+      <div className='game-menu-buttons'>{numOfPlayersButtons}</div>
       <h3>Grid Size</h3>
-      <div className='game-menu-buttons'>
-        <Button type='menu-selection'>4x4</Button>
-        <Button type='menu-selection'>6x6</Button>
-      </div>
+      <div className='game-menu-buttons'>{gridSizeButtons}</div>
       <Button onClick={props.onGameStart} type='menu-start'>
         Start Game
       </Button>
--- src/constants.js
+++ src/constants.js
@@ -40,16 +40,16 @@ const ICONS_ARR = [
   faSun,
 ];
 
-const GAME_GRID_SIZES = {
-  '4x4': 16,
-  '6x6': 36,
-};
-
 const GAME_THEMES = {
   NUMBERS: 'numbers',
   ICONS: 'icons',
 };
 
-const NUM_OF_PLAYERS = 4;
+const GAME_GRID_SIZES = {
+  '4x4': 16,
+  '6x6': 36,
+};
 
-export { GAME_GRID_SIZES, GAME_THEMES, NUM_OF_PLAYERS, ICONS_ARR };
+const MAX_NUM_OF_PLAYERS = 4;
+
+export { GAME_GRID_SIZES, GAME_THEMES, MAX_NUM_OF_PLAYERS, ICONS_ARR };
