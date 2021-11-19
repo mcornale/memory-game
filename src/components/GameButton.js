dd634809fbb9355fd357ca44942cc3359f357569
mcornale <massimiliano.cornale@gmail.com>
2021-11-19 08:09:42 +0100

    Repair GameButton code

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -1,19 +1,10 @@
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateMoves } from '../store/gameSinglePlayerSlice';
 
 const GameButton = (props) => {
   const [isVisible, setIsVisible] = useState(false);
-  const numOfPlayers = useSelector((state) => state.gameSettings.numOfPlayers);
-  const moves = useSelector((state) => state.gameSinglePlayer.moves);
-  const dispatch = useDispatch();
 
   const onClickHandler = () => {
     setIsVisible((prevVisibility) => !prevVisibility);
-    if (numOfPlayers === 1) {
-      if ((moves + 1) % 2 === 0) console.log('copri');
-      dispatch(updateMoves());
-    }
   };
 
   return (
--- src/store/gameSinglePlayerSlice.js
+++ src/store/gameSinglePlayerSlice.js
@@ -29,6 +29,6 @@ const gameSinglePlayerSlice = createSlice({
   },
 });
 
-export const { restartGame, updateTimer, updateMoves } =
+export const { restartGame, updateTimer, updateMoves, addToLastTwoMovesIndex } =
   gameSinglePlayerSlice.actions;
 export default gameSinglePlayerSlice.reducer;
