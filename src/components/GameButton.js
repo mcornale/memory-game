c9f8111d4d0f1b5524776cf14aad3a2371bcdb8d
mcornale <massimiliano.cornale@gmail.com>
2021-11-24 17:46:43 +0100

    Fix HTML validation issues

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -2,15 +2,15 @@ import styles from '../styles/GameButton.module.css';
 
 const GameButton = (props) => {
   return (
-    <button
+    <div
       className={`${styles['game-button']}`}
       {...(!props.isVisible ? { onClick: props.onMoveMade } : {})}
     >
-      <div
+      <button
         className={`${styles['game-button__cover']} ${
           props.isVisible ? styles['game-button__cover--hidden'] : ''
         }`}
-      ></div>
+      ></button>
       <div
         className={`${styles['game-button__content']}  ${
           props.isActive ? styles['game-button__content--active'] : ''
@@ -18,7 +18,7 @@ const GameButton = (props) => {
       >
         {props.children}
       </div>
-    </button>
+    </div>
   );
 };
 
